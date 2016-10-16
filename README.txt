d698f3512a304c51b4493d09ab4d5761ae973559
jlb <jlb@bibi2.bibi.lan>
2016-10-16 20:53:32 +0200

    version 0.6

---
+++ README.txt
@@ -0,0 +1,7 @@
+XML2Contacts for Firefox OS - v0.6
+
+Copyright (c) 2014-2016 Jean Luc Biellmann (contact@alsatux.com)
+
+Restore contacts from the SD-Card. Backups should be made using Contact2XML webapp.
+
+Thanks to Tango Icon (http://openclipart.org/detail/35431/tango-preferences-system-by-warszawianka).
--- js/xml2contacts.js
+++ js/xml2contacts.js
@@ -1,18 +1,21 @@
-// Jean Luc Biellmann - contact@alsatux.com - 20140226 - v0.4
+// Jean Luc Biellmann - contact@alsatux.com - 20140410 - v0.5
 
 var _Log = {
 	clear: function () {
-		var div = document.getElementById('log');
-		div.innerHTML = '';
+		document.getElementById('log').innerHTML = '';
 	},
-	info: function (mess, clear=false) {
-		var div = document.getElementById('log');
-		div.innerHTML += '<p>' + mess + '</p>';
+	push: function (mess, className='') {
+		document.getElementById('log').innerHTML += '<p' + (className.length ? ' class="' + className + '"' : '') + '>' + mess + '</p>';
 	},
-	error: function (mess, clear=false) {
-		var div = document.getElementById('log');
-		div.innerHTML += '<p style="color:red">' + mess + '</p>';
+	info: function (mess) {
+		_Log.push(mess, 'info');
 	},
+	warn: function (mess) {
+		_Log.push(mess, 'warn');
+	},
+	error: function (mess) {
+		_Log.push(mess, 'error');
+	}
 };
 
 var _Storage  = {
@@ -41,6 +44,20 @@ var _Storage  = {
 	}
 };
 
+var _DataURL = {
+	decode2Uint8Array: function (data) {
+		// decode a base64 string
+		var binary = atob(data);
+		// create an array of byte values where each element will be the value of the byte
+		var buffer = [];
+		for (var i=0;i<binary.length;i++)
+			// charCodeAt() method returns the Unicode of the character at the specified index in a string. (H -> 72)
+			buffer.push(binary.charCodeAt(i));
+		// convert the array of byte values into a real typed byte array
+		return new Uint8Array(buffer);
+	}
+};
+
 var _XML2C = {
 
 	xmlnode: null,
@@ -48,6 +65,8 @@ var _XML2C = {
 	counter: 0,
 	json: {},
 	photos: {},
+	cb1_checked: null,
+	cb2_checked: null,
 	
 	info: function (id,mess) {
 		var div = document.getElementById(id);
@@ -55,6 +74,16 @@ var _XML2C = {
 	},
 	
 	init: function () {
+		var but1 = document.getElementById('but1');
+		but1.addEventListener("click", _XML2C.restore);
+		var but2 = document.getElementById('but2');
+		but2.addEventListener("click", _XML2C.clearAll);
+		var but3 = document.getElementById('but3');
+		but3.addEventListener("click", _XML2C.show_part1);
+		_XML2C.reset();
+	},
+	
+	reset: function () {		
 		_Storage.ls('sdcard', 'contacts2xml', function (files) {
 			var select = document.getElementById('sel1');
 			select.innerHTML = '';
@@ -79,39 +108,71 @@ var _XML2C = {
 		return true;
 	},
 
+	show_part1: function () {
+		document.getElementById('part1').style.display = '';
+		document.getElementById('part2').style.display = 'none';	
+		document.getElementById('but3').style.display = 'none';
+		_Log.clear();
+		document.getElementById('count').innerHTML = '';
+		window.scrollTo(0,0);
+	},
+	
+	show_and_reset_part2: function () {
+		document.getElementById('part1').style.display = 'none';
+		document.getElementById('part2').style.display = '';		
+		document.getElementById('but3').style.display = 'none';
+		_Log.clear();
+		document.getElementById('count').innerHTML = '';
+		window.scrollTo(0,0);
+	},
+
 	clearAll: function () {
 		if (confirm('Delete all contacts ? (no way back !)')) {
-			_Log.clear();
-			document.getElementById('counter').innerHTML = '';
+			_XML2C.show_and_reset_part2();
 			_Log.info('Deleting all contacts...');
-			document.getElementById('chooser').style.display = 'none';
-			document.getElementById('cleaner').style.display = 'none';
 			var req = window.navigator.mozContacts.clear();
 			req.onsuccess = function () {
 				_Log.clear();
-				_Log.info("Contacts removed !");
-				document.getElementById('chooser').style.display = '';
-				document.getElementById('cleaner').style.display = '';
+				_Log.warn("Contacts removed !");
+				document.getElementById('but3').style.display = '';
 			};
 			req.onerror = function () {
 				_Log.clear();
 				_Log.error("Removing all contacts failed.");
-				document.getElementById('chooser').style.display = '';
-				document.getElementById('cleaner').style.display = '';
+				document.getElementById('but3').style.display = '';
 			};
 		}
 	},
 
 	restore: function () {
-		_Log.clear();
-		document.getElementById('counter').innerHTML = '';
-		_Log.info('Restoring contacts from sd-card...');
+		_XML2C.show_and_reset_part2();
+		// read current state
+		_XML2C.cb1_checked = document.getElementById('cb1').checked;
+		_XML2C.cb2_checked = document.getElementById('cb2').checked;
 		var select = document.getElementById('sel1');
 		var index = select.options.selectedIndex;
-		if (index!=-1) {
-			document.getElementById('chooser').style.display = 'none';
-			document.getElementById('cleaner').style.display = 'none';
-			var filename = select.options[index].value;
+		if (index==-1)
+			return _Log.error('No file selected !');
+		var filename = select.options[index].value;
+		var backup_date = filename.replace('contacts2xml/','').replace('.xml','');
+		backup_date = parseInt(backup_date.substr(0,8),10);
+		if (backup_date<20140417) {
+			if (_XML2C.cb2_checked && !confirm('It seems your backup has been made with a  version of Cockatoo < 0.5, so you should rename your old photos from "XXX.jpg" to "XXX.1.jpg" before restoring your contacts. Continue ?')) {
+				_Log.info('Program aborted !');
+				_XML2C.stop_restore();
+				return false;
+			}
+		}			
+		if (_XML2C.cb1_checked || _XML2C.cb2_checked || confirm('Do you really want to ignore all photos ?')) {
+			if (_XML2C.cb1_checked && _XML2C.cb2_checked) {
+				_Log.info('Get photos from data_uri and sd-card...');
+			} else {
+				if (_XML2C.cb1_checked)
+					_Log.info('Get photos from data_uri...');
+				if (_XML2C.cb2_checked)
+					_Log.info('Get photos from sd-card...');
+			}
+			// read file from sd-card
 			var medium = navigator.getDeviceStorage('sdcard');
 			var req = medium.get(filename);
 			req.onsuccess = function () {
@@ -121,33 +182,40 @@ var _XML2C = {
 				_Log.error("Unable to read file: " + this.error);
 				_XML2C.stop_restore();
 			}
-		} else {
-			_Log.error('No file selected !');
+		} else {
+			_Log.info('Program aborted !');
+			_XML2C.stop_restore();			
 		}
 	},
-	
+
 	stop_restore: function () {
-		document.getElementById('chooser').style.display = '';
-		document.getElementById('cleaner').style.display = '';		
+		document.getElementById('but3').style.display = '';
 		_Log.info('Finished !');
 	},
 
 	album: function (file, filename) {
-		var re = new RegExp('^.*contacts2xml/([0-9]{14}).xml$','');
-		var m = filename.match(re);
-		var photopath = 'contacts2xml/' + m[1];
-		_Storage.ls('sdcard', photopath, function (files) {
-			_XML2C.photos = {};
-			var re2 = new RegExp('^.*' + photopath + '/(.*).jpg$','');
-			for (var i=0;i<files.length;i++) {
-				var m2 = (files[i].name).match(re2);
-				if (m2!=null) {
-					var photoid = m2[1];
-					_XML2C.photos[photoid] = files[i].name;
-				}
-			}
+		_XML2C.photos = {};
+		if (!_XML2C.cb2_checked) {
+			// no external JPG files
 			_XML2C.handle(file, filename);
-		});								
+		} else {
+			var re = new RegExp('^.*contacts2xml/([0-9]{14})\\.xml$','');
+			var m = filename.match(re);
+			var photopath = 'contacts2xml/' + m[1];
+			_Storage.ls('sdcard', photopath, function (files) {
+				var re2 = new RegExp('^.*' + photopath + '/(.*)\\.[0-9]+\\.jpg$','');
+				for (var i=0;i<files.length;i++) {
+					var m2 = (files[i].name).match(re2);
+					if (m2!=null) {
+						var photoid = m2[1];
+						if (!(photoid in _XML2C.photos))
+							_XML2C.photos[photoid] = [];
+						_XML2C.photos[photoid].push(files[i].name);
+					}
+				}
+				_XML2C.handle(file, filename);
+			});							
+		}	
 	},
 
 	handle: function (file, filename) {
@@ -155,8 +223,10 @@ var _XML2C = {
 		reader.onload = function(e) {
 			var text = (reader.result).replace('<?xml version="1.0" encoding="UTF-8"?>','');
 			var parser = new DOMParser();
-			var xmldoc = parser.parseFromString(text, 'text/xml');
-			_XML2C.xmlnode = xmldoc.getElementsByTagName('contact');
+			var xml_doc = parser.parseFromString(text, 'text/xml');
+			// avoid firefox to split long datauri in parts...
+			//xml_doc.normalize();
+			_XML2C.xmlnode = xml_doc.getElementsByTagName('contact');
 			_XML2C.counter = 0;
 			_XML2C.i = 0;
 			_XML2C.loop();
@@ -170,20 +240,42 @@ var _XML2C = {
 			return;
 		}
 		_XML2C.json = _XML2C.xml2json(_XML2C.xmlnode[_XML2C.i++]);
-		// retrieve photo using old contact id
-		if (_XML2C.json.id!=undefined && _XML2C.photos[_XML2C.json.id]!=undefined) {
-			var storage = navigator.getDeviceStorage('sdcard');
-			var req = storage.get(_XML2C.photos[_XML2C.json.id]);
-			req.onsuccess = function () {
-				var pict = this.result;
-				_XML2C.json.photo = [new Blob([pict], {type: 'image/jpg'})];
-				_XML2C.record(_XML2C.json);
-			}
-			req.onerror = function () {
-				_Log.error('Unable to read photo ' + _XML2C.json.id);
-			}
-		} else {
+		if (!_XML2C.cb1_checked) { 
+			// remove photos found in data-uri
+			if ('photo' in _XML2C.json)
+				delete _XML2C.json.photo;
+		}
+		if (!_XML2C.cb2_checked) { 
+			// no external JPG files
 			_XML2C.record(_XML2C.json);
+		} else {
+			if (!('id' in _XML2C.json) || !(_XML2C.json.id in _XML2C.photos)) {
+				// no id given or no photo available
+				_XML2C.record(_XML2C.json);
+			} else {
+				// retrieve photo using old contact id
+				_XML2C.loop_photos(_XML2C.json, 0);
+			}
+		}
+	},
+
+	loop_photos: function (json, index) {
+		if (index>=_XML2C.photos[json.id].length)
+			return _XML2C.record(json);
+		var filename = _XML2C.photos[json.id][index];
+		var storage = navigator.getDeviceStorage('sdcard');
+		var req = storage.get(filename);
+		req.onsuccess = function () {
+			var data = this.result;
+			if (!('photo' in json))
+				json.photo = [];
+			json.photo.push( new Blob([data], {type: 'image/jpeg'}) );
+			_XML2C.loop_photos(json, index+1);
+		}
+		req.onerror = function () {
+			// ignore bad photos
+			_Log.error('Unable to read photo ' + _XML2C.json.id + '.jpg');
+			_XML2C.loop_photos(json, index+1);
 		}
 	},
 
@@ -195,7 +287,7 @@ var _XML2C = {
 		var req = window.navigator.mozContacts.save(person);
 		req.onsuccess = function () {
 			_XML2C.counter++;
-			document.getElementById('counter').innerHTML = _XML2C.counter + ' contacts restored...';
+			document.getElementById('count').innerHTML = _XML2C.counter + ' contacts restored...';
 			_XML2C.loop();
 		}
 		req.onerror = function () {
@@ -209,18 +301,29 @@ var _XML2C = {
 		if (xml.hasChildNodes()) {
 			for(var i=0; i<xml.childNodes.length; i++) {
 				var item = xml.childNodes.item(i);
-				var nodeName = item.nodeName;
-				// ignore all read only fields but the id (will be used retrieve photo later)
-				if (nodeName.match(/(published|updated)/))
-					continue;
-				if (typeof(nodeName)!='undefined') {
-					if (nodeName.match(/(tel|email|url)/)) {
+				if ('nodeName' in item) {
+					var nodeName = item.nodeName;
+					// ignore all read only fields but the id (will be used retrieve photo later)
+					if (nodeName.match(/^(published|updated)$/)!=null)
+						continue;
+					if (nodeName.match(/^photo$/)!=null) {
+						var xml_serializer = new XMLSerializer();
+				      var data = xml_serializer.serializeToString(item);
+				      var m = data.match(/^<photo>data:image\/jpe?g;base64,(([0-9a-zA-Z+\/]{4})*(([0-9a-zA-Z+\/]{2}==)|([0-9a-zA-Z+\/]{3}=))?)<\/photo>$/);
+						if (m!=null) {
+							if (!(nodeName in obj))
+								obj[nodeName] = [];
+							var blobdata = _DataURL.decode2Uint8Array(m[1]);
+							obj[nodeName].push( new Blob([blobdata], {type: 'image/jpeg'}) );
+						}
+					}
+					else if (nodeName.match(/^(tel|email|url)$/)!=null) {
 						var child_obj = {};
 						for(var j = 0; j<item.childNodes.length; j++) {
 							var subitem = item.childNodes.item(j);
-							if (subitem.childNodes[0]!=undefined && subitem.childNodes[0].nodeValue!=undefined) {
+							if ('nodeName' in subitem && 'childNodes' in subitem && subitem.childNodes.length && 'nodeValue' in subitem.childNodes[0]) {
 								if (subitem.nodeName=='type') {
-									if (typeof(child_obj['type'])=='undefined')
+									if (!('type' in child_obj))
 										child_obj['type'] = [];
 									child_obj['type'].push(subitem.childNodes[0].nodeValue);
 								}
@@ -228,8 +331,8 @@ var _XML2C = {
 									child_obj['value'] = subitem.childNodes[0].nodeValue;
 							}
 						}
-						if (child_obj['value']!=undefined && child_obj['value'].length) {
-							if (typeof(obj[nodeName])=='undefined')
+						if ('value' in child_obj && child_obj['value'].length) {
+							if (!(nodeName in obj))
 								obj[nodeName] = [];
 							obj[nodeName].push(child_obj);
 						}
@@ -238,33 +341,33 @@ var _XML2C = {
 						var child_obj = {};
 						for(var j = 0; j<item.childNodes.length; j++) {
 							var subitem = item.childNodes.item(j);
-							if (subitem.childNodes[0]!=undefined && subitem.childNodes[0].nodeValue!=undefined) {
+							if ('nodeName' in subitem && 'childNodes' in subitem && subitem.childNodes.length && 'nodeValue' in subitem.childNodes[0]) {
 								if (subitem.nodeName=='type') {
-									if (typeof(child_obj['type'])=='undefined')
+									if (!('type' in child_obj))
 										child_obj['type'] = [];
 									child_obj['type'].push(subitem.childNodes[0].nodeValue);
 								}
 								else if (subitem.nodeName=='pref')
 									child_obj['pref'] = subitem.childNodes[0].nodeValue=='true' ? true : false;
-								else if (subitem.nodeName.match(/(streetAddress|locality|region|postalCode|countryName)/))
+								else if (subitem.nodeName.match(/^(streetAddress|locality|region|postalCode|countryName)$/)!=null)
 									child_obj[subitem.nodeName] = subitem.childNodes[0].nodeValue;
 							}
 						}
-						if (typeof(obj[nodeName])=='undefined')
+						if (!(nodeName in obj))
 							obj[nodeName] = [];
 						obj[nodeName].push(child_obj);
 					}
 					else {
-						if (item.childNodes[0]!=undefined && item.childNodes[0].nodeValue!=undefined) {
-							if (nodeName.match(/(name|honorificPrefix|givenName|additionalName|familyName|honorificSuffix|nickname|category|org|jobTitle|note|key)/)) {
-								if (typeof(obj[nodeName])=='undefined')
+						if ('childNodes' in item && item.childNodes.length && 'nodeValue' in item.childNodes[0]) {
+							if (nodeName.match(/^(name|honorificPrefix|givenName|additionalName|familyName|honorificSuffix|nickname|category|org|jobTitle|note|key)$/)!=null) {
+								if (!(nodeName in obj))
 									obj[nodeName] = [];
 								obj[nodeName].push(item.childNodes[0].nodeValue);
 							}
-							else if (nodeName.match(/(id|sex|genderIdentity)/)) {
+							else if (nodeName.match(/^(id|sex|genderIdentity)$/)!=null) {
 								obj[nodeName] = item.childNodes[0].nodeValue;
 							}
-							else if (nodeName.match(/(bday|anniversary)/)) {
+							else if (nodeName.match(/^(bday|anniversary)$/)!=null) {
 								obj[nodeName] = new Date(item.childNodes[0].nodeValue);
 							}
 						}
@@ -279,8 +382,4 @@ var _XML2C = {
 
 window.onload = function () {
 	_XML2C.init();
-	var but1 = document.getElementById('but1');
-	but1.addEventListener("click", _XML2C.restore);
-	var but2 = document.getElementById('but2');
-	but2.addEventListener("click", _XML2C.clearAll);
 }
